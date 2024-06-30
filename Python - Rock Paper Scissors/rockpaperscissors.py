403de51b2ac9275575c908c4e397ceb7afe4f75b
Sarah <149964758+sarahbtn96@users.noreply.github.com>
2024-06-30 04:29:45 +0100

    Update rockpaperscissors.py

--- Python - Rock Paper Scissors/rockpaperscissors.py
+++ Python - Rock Paper Scissors/rockpaperscissors.py
@@ -3,6 +3,8 @@ import random
 print ("Let's play Rock, Paper Scissors!\nRock smashes Scissors. Paper covers Rock. Scissors cut Paper.")
 
 options = ["Rock", "Paper", "Scissors"]
+player_score = 0
+computer_score = 0
 
 while True: 
     user_choice = input("Choose Rock, Paper or Scissors: ")
@@ -18,20 +20,30 @@ while True:
     elif user_choice == "Rock" and computer_choice == "Scissors":
 
         print("You win!")
+        player_score += 1
 
     elif user_choice == "Paper" and computer_choice == "Rock":
 
         print("You win!")
+        player_score += 1
 
     elif user_choice == "Scissors" and computer_choice == "Paper":
 
         print("You win!")
+        player_score += 1
 
     else:
 
         print("Computer wins!")
-
+        computer_score += 1
+        
+    print("Scores:")
+    print("Computer:", computer_score)
+    print("Player:", player_score)
+    
     repeat = input("\nPlay again? (yes/no): ")
     if repeat.lower() != "yes":
         print("Thanks for playing!")
         break
+        
+    
